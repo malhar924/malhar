d9c1ed9b0bf4475d80e6444f64475b8705cbe64f
malhar924 <83411461+malhar924@users.noreply.github.com>
2021-05-31 19:26:33 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,25 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+        this.pointB=pointB;
+    }
+fly(){
+this.chain.bodyA=null;
+
+}
+    display(){
+        if(this.chain.bodyA){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+    }    
+}
